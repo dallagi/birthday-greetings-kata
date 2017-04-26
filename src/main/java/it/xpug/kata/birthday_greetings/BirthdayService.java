c713a84cc5c336768b4c140edc6da4393dcc49e6
Alessio Coser <alessio.coser@xpeppers.com>
2017-04-26 14:24:03 +0200

    delegate send mail logic to mailSender collaborator

--- src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
+++ src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
@@ -5,15 +5,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 
-import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
 import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 public class BirthdayService {
+
+	private final MailSender mailSender = new MailSender();
 
 	public void sendGreetings(String fileName, XDate xDate, String smtpHost, int smtpPort) throws IOException, ParseException, AddressException, MessagingException {
 		BufferedReader in = new BufferedReader(new FileReader(fileName));
@@ -32,20 +29,6 @@ public class BirthdayService {
 	}
 
 	protected void sendMessage(String smtpHost, int smtpPort, String sender, String subject, String body, String recipient) throws AddressException, MessagingException {
-		// Create a mail session
-		java.util.Properties props = new java.util.Properties();
-		props.put("mail.smtp.host", smtpHost);
-		props.put("mail.smtp.port", "" + smtpPort);
-		Session session = Session.getInstance(props, null);
-
-		// Construct the message
-		Message msg = new MimeMessage(session);
-		msg.setFrom(new InternetAddress(sender));
-		msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-		msg.setSubject(subject);
-		msg.setText(body);
-
-		// Send the message
-		Transport.send(msg);
+		mailSender.sendMessage(smtpHost, smtpPort, sender, subject, body, recipient);
 	}
 }
