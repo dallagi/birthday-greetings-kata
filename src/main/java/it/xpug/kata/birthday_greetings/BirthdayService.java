94f31e04babdf9920a8164b9fa7b3ea4f62ac177
Alessio Coser <alessio.coser@xpeppers.com>
2017-05-09 14:30:26 +0200

    delegate getEmployee to new Class FileEmployeeRepository

--- src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
+++ src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
@@ -1,10 +1,7 @@
 package it.xpug.kata.birthday_greetings;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.mail.MessagingException;
@@ -18,7 +15,7 @@ public class BirthdayService {
 	}
 
 	public void sendGreetings(String fileName, XDate xDate, Sender sender) throws IOException, ParseException, MessagingException {
-		for (Employee employee: getEmployees(fileName) ) {
+		for (Employee employee: getEmployees(fileName)) {
 			if (employee.isBirthday(xDate)) {
 				sender.sendGreetingsTo(employee);
 			}
@@ -26,16 +23,8 @@ public class BirthdayService {
 	}
 
 	protected List<Employee> getEmployees(String fileName) throws IOException, ParseException {
-		List<Employee> employees = new ArrayList<Employee>();
-		BufferedReader in = new BufferedReader(new FileReader(fileName));
-		String str = in.readLine(); // skip header
-
-		while ((str = in.readLine()) != null) {
-			String[] employeeData = str.split(", ");
-			Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
-			employees.add(employee);
-		}
-		return employees;
+		FileEmployeesRepository fileEmployeesRepository = new FileEmployeesRepository();
+		return fileEmployeesRepository.getEmployees(fileName);
 	}
 
 }
---
+++ src/main/java/it/xpug/kata/birthday_greetings/FileEmployeesRepository.java
@@ -0,0 +1,26 @@
+package it.xpug.kata.birthday_greetings;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileEmployeesRepository {
+    public FileEmployeesRepository() {
+    }
+
+    protected List<Employee> getEmployees(String fileName) throws IOException, ParseException {
+        List<Employee> employees = new ArrayList<Employee>();
+        BufferedReader in = new BufferedReader(new FileReader(fileName));
+        String str = in.readLine(); // skip header
+
+        while ((str = in.readLine()) != null) {
+            String[] employeeData = str.split(", ");
+            Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
+            employees.add(employee);
+        }
+        return employees;
+    }
+}
